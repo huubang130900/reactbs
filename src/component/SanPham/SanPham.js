1aa136e0caeddf71f98478e806d0998650fef29f
huubang130900 <huubang130900@gmail.com>
2019-10-24 08:25:56 +0700

    sanpham

---
+++ src/component/SanPham/SanPham.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SanPham extends  React.Component{
+    render(){
+    return (
+        <div>
+            <p>Mã Sản Phẩm: {this.props.MSP}, Tên Sản Phẩm: {this.props.TSP}, Giá: {this.props.GIA}</p>
+            <p>{this.props.children}</p>
+        </div>
+    );
+ }
+}
+
+export default SanPham
